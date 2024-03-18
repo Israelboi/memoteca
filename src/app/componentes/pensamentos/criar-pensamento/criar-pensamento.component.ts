02e9de9f48e7d9e0ba3dd1f89a871c8f41c61d34
Israel <ysrraeu@gmail.com>
2024-03-18 19:26:26 -0300

    Implementação de componentes

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../../pesamentos/pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -7,16 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamentoComponent implements OnInit {
 
-  pensamento = {
-    id: '1',
-    conteudo: 'Aprendendo Angular',
-    autoria: 'Dev',
-    modelo: ''
+  pensamento :Pensamento= {
+    conteudo: '',
+    autoria: '',
+    modelo: 'modelo1'
   }
 
-  constructor() { }
+  constructor(
+    private service:PensamentoService,
+    private router:Router
+    ) { }
 
   ngOnInit(): void {
+  }
+
+  salvarPensamento(){
+    this.service.criar(this.pensamento).subscribe(()=>{
+      this.router.navigate(['/listarPensamento'])
+    })
+  }
+
+  cancelarPensamento(){
+    this.router.navigate(['/listarPensamento'])
   }
 
 }
